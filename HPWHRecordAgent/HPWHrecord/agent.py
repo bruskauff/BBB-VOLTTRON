0f973aa5c7dc96051cf764833aa126f6527892f5
bruskauff <bruskauff@gmail.com>
2015-11-02 15:02:58 -0700

    Recording agent framwork complete. Agent needs a little more work to be operational.

---
+++ HPWHRecordAgent/HPWHrecord/agent.py
@@ -0,0 +1,202 @@
+#__________________________________Copyright__________________________________#
+'''
+Copyright (c) 2013, Battelle Memorial Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in
+   the documentation and/or other materials provided with the
+   distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation
+are those of the authors and should not be interpreted as representing
+official policies, either expressed or implied, of the FreeBSD
+Project.
+
+This material was prepared as an account of work sponsored by an
+agency of the United States Government.  Neither the United States
+Government nor the United States Department of Energy, nor Battelle,
+nor any of their employees, nor any jurisdiction or organization that
+has cooperated in the development of these materials, makes any
+warranty, express or implied, or assumes any legal liability or
+responsibility for the accuracy, completeness, or usefulness or any
+information, apparatus, product, software, or process disclosed, or
+represents that its use would not infringe privately owned rights.
+
+Reference herein to any specific commercial product, process, or
+service by trade name, trademark, manufacturer, or otherwise does not
+necessarily constitute or imply its endorsement, recommendation, or
+favoring by the United States Government or any agency thereof, or
+Battelle Memorial Institute. The views and opinions of authors
+expressed herein do not necessarily state or reflect those of the
+United States Government or any agency thereof.
+
+PACIFIC NORTHWEST NATIONAL LABORATORY
+operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+under Contract DE-AC05-76RL01830
+'''
+#_____________________________________________________________________________#
+
+
+
+#____________________________________About____________________________________#
+'''
+Brian Ruskauff
+National Renewable Energy Labs (NREL)
+11/1/15
+Referenced from Kathleen Genger's agent code for dehumidifier control
+
+
+This scrip records pertinent data from heat pump water heater (HPWH) testing in a space dilineated text file.
+
+INPUT 
+	- signal from UtilityAgent with [costlevel, cost]
+		- costlevel is 'high', 'medium', or 'low'
+		- cost is a float in dollars
+	- signals from HPWHUserAgent with:
+		- topic = 'user/temp'
+		- message = [old_temp, new_temp]
+		- topic = 'user/state'
+		- message = [old_state, new_state]
+		- topic = 'user/sim_temp'
+		- message = [up_temp, low_temp]
+OUTPUT
+	- when outputting status
+		- topic = 'control/status'
+		- message = [state, mode, desired_temp]
+	- logs the inputted cost level, cost, and heating response.
+'''
+#_____________________________________________________________________________#
+
+
+
+#____________________________________Setup____________________________________#
+
+# Import Python dependencies
+import logging, sys, time, settings, datetime
+
+# Not sure I actually need this
+from zmq.utils import jsonapi
+
+# Import VOLTTRON dependencies
+from volttron.platform.agent import BaseAgent, PublishMixin, periodic, utils, matching
+
+# Enable information and debug logging
+utils.setup_logging()
+_log = logging.getLogger(__name__)
+#_____________________________________________________________________________#
+
+
+
+#____________________________________Agent____________________________________#  
+
+# Blink an LED based off of input from user and demand agents
+class HPWHRecordAgent(PublishMixin, BaseAgent):
+
+	def __init__(self, config_path, **kwargs):
+		super(HPWHRecordAgent, self).__init__(**kwargs)
+
+		# load library from config file
+		self.config = utils.load_config(config_path)
+
+	# Additional Setup
+	def setup(self):
+		# Publish message from config file, usually a setup msg
+		_log.info(self.config['message'])
+		super(HPWHRecordAgent, self).setup()
+
+		# Set up txt file to save data
+		# Define the start of test
+		now = datetime.datetime.now()
+		# Define the file as test_isotime
+		filename = 'test' + now.isoformat()
+		# Open the file
+		target = open(filename, 'w')
+		# Make sure file is clear
+		target.truncate()
+		# Write Header
+		target.write('Brian Ruskauff\nNREL\nHPWH Control Test\n%s\n\n' %now)
+		target.write('TimeStamp    Temp_Lower(F)    Temp_Upper(F)    '
+				'Desired_Temp(F)    Power(KW)    Status\n\n')
+
+	# Check for Measurement Input - Temperature
+	@matching.match_start('measure/temp')
+	def tank_temp(self, topic, headers, message, match):
+		# Measure Agent publishes message = [up_temp, low_temp]
+		temp_info = jsonapi.loads(message[0])
+		try:
+			self.up_temp = temp_info[0]
+			self.low_temp = temp_info[1]
+		except:
+			self.up_temp = 'NaN'
+			self.low_temp = 'NaN'
+	
+
+	# Check for User Input - Temperature
+	@matching.match_start('user/temp')
+	# Define the desired temperature
+	def define_temp(self, topic, headers, message, match):
+		# User Agent published message = [old_temp, new_temp]
+		temp_info = jsonapi.loads(message[0])
+		self.desired_temp = temp_info[1]
+
+	@periodic(settings.record_int)
+	# Write Data to txt file
+	def write_date(self):
+		now = datetime.datetime.now()
+		# Write Time Stamp
+		target.write('%s:%s:%s      ' %(now.hour, now.minute, now.second))
+		# Write Lower Tank Temperature
+		target.write(str(round(self.low_temp, 3)) + '          ')
+		# Write Upper Tank Temperature
+		target.write(str(round(self.up_temp, 3)) + '          ')
+		# Write Desired Temperature
+		target.write(str(round(self.desired_temp, 3)) + '              ')
+		# Write Power Consumption
+		target.write(str(round(self.power, 3)) + '             ')
+		# Write Status
+		target.write(self.status)
+		target.write('\n')
+
+	@periodic(settings.pub_int)
+	# Publish the current mode of operation and tank temperature
+	def send_status(self):
+		self.publish_json('control/status', {}, (self.state, self.mode, 
+				self.desired_temp))
+#_____________________________________________________________________________#
+
+
+
+#________________________________Platform Stuff_______________________________#
+
+# Enable Agent to parse arguments on the command line by the agent launcher
+def main(argv=sys.argv):
+    # Main method called by the platform.
+    utils.default_main(HPWHRecordAgent,
+                       description='HPWH Record Agent',
+                       argv=argv)
+
+if __name__ == '__main__':
+    # Entry point for script
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        pass
