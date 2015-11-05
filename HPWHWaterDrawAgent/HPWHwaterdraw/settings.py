f84ccc22ed9da20438a4cc29417a2936399f187e
bruskauff <bruskauff@gmail.com>
2015-11-05 12:32:34 -0700

    Established basic framework for water draw agent. Not-Functional.

---
+++ HPWHWaterDrawAgent/HPWHwaterdraw/settings.py
@@ -0,0 +1,7 @@
+'''
+Mode interval affects how often the agent will check if it needs to change its mode of operation. Publish interval affects how often it publishes mode and temperature information. Record interval affects how often it records data to the text file.
+'''
+
+mode_int = 5
+pub_int = 5
+record_int = 5
