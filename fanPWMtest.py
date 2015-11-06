5162efc750b8b398885deda9956dca1b5aca1166
Demo User <debian@beaglebone.localdomain>
2015-11-06 17:04:20 +0000

    updated typical message to 'typ:85%'. A duty cycle of 85% makes the fans run at about 8 watts.

--- fanPWMtest.py
+++ fanPWMtest.py
@@ -5,12 +5,12 @@ FAN = 'PWM1A'#Pin P9.14
 # Initialize pin at 0 duty cycle
 analogWrite(FAN, 0)
 # Set frequency to 30,000 Hz
-pwmFrequency(FAN, 30000)
+pwmFrequency(FAN, 15000)
 
 while True:
 	# Input percent speed
 	percent = float(raw_input(
-			'\nWhat percent should I run at?\nTyp: 80%.\n'
+			'\nWhat percent should I run at?\nTyp: 85%.\n'
 			'>>>'))
 	duty = 255*percent/100
 	# Set pwm frequency
