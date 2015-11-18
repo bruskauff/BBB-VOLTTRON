dbb183bb2abff8fde37ac51c6dbf5b0ff86edd2c
Brian <bruskauff@gmail.com>
2015-11-17 19:05:09 -0700

    Update settings.py

--- HPWHControlAgent/HPWHcontrol/settings.py
+++ HPWHControlAgent/HPWHcontrol/settings.py
@@ -1,7 +1,6 @@
 '''
-Mode interval affects how often the agent will check if it needs to change its mode of operation. Publish interval affects how often it publishes mode and temperature information. Record interval affects how often it records data to the text file.
+Mode interval affects how often the agent will check if it needs to change its mode of operation. Publish interval affects how often it publishes mode and temperature information.
 '''
 
 mode_int = 5
 pub_int = 5
-record_int = 5
